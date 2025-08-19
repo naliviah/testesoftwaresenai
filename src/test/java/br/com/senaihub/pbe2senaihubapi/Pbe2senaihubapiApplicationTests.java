566624a08a831315bcb86a46633c615eec98bc36
Aninha <ana.l.silva158@aluno.senai.br>
2025-08-19 08:40:33 -0300

    commit inicial

---
+++ src/test/java/br/com/senaihub/pbe2senaihubapi/Pbe2senaihubapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senaihub.pbe2senaihubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbe2senaihubapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
